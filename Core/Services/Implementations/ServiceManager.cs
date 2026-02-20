568b67c8274066e53a77bab75249c77b59cc9508
Ali-Al-husseiny <ali.ahmed.software.engineer@gmail.com>
2026-02-20 08:35:10 +0200

    Add BasketService to IServiceManager

--- Core/Services/Implementations/ServiceManager.cs
+++ Core/Services/Implementations/ServiceManager.cs
@@ -4,12 +4,16 @@ using Services.Abstraction;
 
 namespace Services.Implementations;
 
-public class ServiceManager(IUnitOfWork _uniOfWork, IMapper _mapper) : IServiceManager
+public class ServiceManager(IUnitOfWork _uniOfWork, IMapper _mapper, IBasketRepository _basketRepo) : IServiceManager
 {
     private readonly Lazy<IProductService> _productService =
         new Lazy<IProductService>(() =>
             new ProductService(_uniOfWork, _mapper)); // Lazy initialization for ProductService
 
 
+    private readonly Lazy<IBasketService> _basketService =
+        new Lazy<IBasketService>(() => new BasketService(_basketRepo, _mapper));
+
     public IProductService ProductService => _productService.Value;
+    public IBasketService BasketService => _basketService.Value;
 }
